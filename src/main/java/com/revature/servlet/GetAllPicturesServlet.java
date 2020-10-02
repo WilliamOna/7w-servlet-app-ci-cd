eb30e5d4cadb23f34ad8ab73c5a2a72a645dcbea
William Ona <william.ona@revature.com>
2020-10-02 09:43:42 -0500

    update code

--- src/main/java/com/revature/servlet/GetAllPicturesServlet.java
+++ src/main/java/com/revature/servlet/GetAllPicturesServlet.java
@@ -26,6 +26,7 @@ public class GetAllPicturesServlet extends HttpServlet {
 		pictures.add(55);
 		pictures.add(13);
 		pictures.add(42);
+		pictures.add(7);
 		PrintWriter pw = resp.getWriter();
 //		pw.println("secret message");
 		pw.println(om.writeValueAsString(pictures));
